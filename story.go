eebd957598e2b7b518ef181cfad83379c236a050
David Braley <david.braley@gmail.com>
2019-07-18 21:08:38 -0400

    Building the http.Handler

--- story.go
+++ story.go
@@ -2,8 +2,51 @@ package gophercises_cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Choose Your Own Adventure</title>
+</head>
+<body>
+	<h1>{{.Title}}</h1> <!-- package 'html/template' golang.org/pkg/html/template -->
+	{{range .Paragraphs}}
+		<p>{{.}}</p>
+	{{end}}
+	<ul>
+		{{range .Options}}
+			<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+		{{end}}
+	</ul>
+</body>
+</html>`
+
+func NewHandler(s Story) http.Handler { // interface return here for the purpose of explicitly defining the purpose of this function
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 func JsonStory(r io.Reader) (Story, error)  {
 	d := json.NewDecoder(r)
